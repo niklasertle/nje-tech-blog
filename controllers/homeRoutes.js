23d487638d50aca85d3cb56e00d45db72e7e073d
Niklas Ertle <hockeytertle@gmail.com>
2021-09-25 16:55:24 -0600

    Fixed homepage route, added simple get requests for posts and users, and added links to the main.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,9 +6,34 @@ const withAuth = require('../utils/withAuth');
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({include: User});
-        const posts = postData.map(post => post.get({plain:true}));
-        res.render('homepage', posts);
+        const posts = postData.map((post) => post.get({plain:true}));
+        res.render('homepage', {posts, logged_in: req.session.logged_in});
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+// Sends the login page
+router.get('/', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Sends the dashboard page
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {id: req.session.user_id},
+            include: User
+        });
+        const posts = postData.map((post) => post.get({plain:true}));
+        res.render('dashboard', {posts, logged_in: req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
+const routes = require('./controllers')
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -28,9 +28,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
